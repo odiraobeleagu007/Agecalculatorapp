8c176f6219fd93e2b868ce0e187174390c309da4
Odira Obeleagu <odira.obeleagu@ccsnl.com>
2023-07-05 11:43:38 +0100

    First commit

---
+++ Age calculator.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+    function age() {
+        var d1 = document.getElementById('date').value;
+        var m1 = document.getElementById('month').value;
+        var y1 = document.getElementById('year').value;
+    }
+</script>
