f0414759fdab4fcaee4a1557d61308ee0648485a
odiraobeleagu007 <124698261+odiraobeleagu007@users.noreply.github.com>
2023-07-05 11:51:13 +0100

    Delete Age calculator.js

--- Age calculator.js
+++
@@ -1,7 +0,0 @@
-<script type="text/javascript">
-    function age() {
-        var d1 = document.getElementById('date').value;
-        var m1 = document.getElementById('month').value;
-        var y1 = document.getElementById('year').value;
-    }
-</script>
